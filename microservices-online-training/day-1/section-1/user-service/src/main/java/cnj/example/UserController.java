cd90ce1237fd77cbbc7ba308e2199ca08c89d745
kbastani <kb@socialmoon.com>
2018-03-15 12:06:42 -0700

    Day 1 section 1

---
+++ microservices-online-training/day-1/section-1/user-service/src/main/java/cnj/example/UserController.java
@@ -0,0 +1,23 @@
+package cnj.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+}
