218b3f32105387bd995e0abc407a52db590dedde
Josh Long <starbuxman@gmail.com>
2018-07-01 17:41:18 -0300

    section3

--- oreilly-july-2018-training/day-1/section-3/reservation-client/src/main/java/com/example/reservationclient/ReservationClientApplication.java
+++ oreilly-july-2018-training/day-1/section-3/reservation-client/src/main/java/com/example/reservationclient/ReservationClientApplication.java
@@ -15,16 +15,20 @@ import org.springframework.cloud.netflix.hystrix.HystrixCommands;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.reactive.function.client.ClientResponse;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
@@ -36,64 +40,71 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 @EnableCircuitBreaker
 public class ReservationClientApplication {
 
-	@Bean
-	RedisRateLimiter redisRateLimiter() {
-		return new RedisRateLimiter(5, 7);
-	}
+		@Bean
+		RedisRateLimiter redisRateLimiter() {
+				return new RedisRateLimiter(5, 7);
+		}
 
-	@Bean
-	ReactiveUserDetailsService authentication() {
-		return new MapReactiveUserDetailsService(
-				User.withDefaultPasswordEncoder()
+		@Bean
+		ReactiveUserDetailsService authentication() {
+				return new MapReactiveUserDetailsService(
+					User.withDefaultPasswordEncoder()
 						.username("user")
 						.password("password")
 						.roles("USER")
 						.build()
-		);
-	}
+				);
+		}
 
-	@Bean
-	WebClient webClient(LoadBalancerExchangeFilterFunction eff) {
-		return WebClient
-				.builder()
-				.filter(eff)
-				.build();
-	}
-
-	@Bean
-	RouterFunction<ServerResponse> routes(WebClient client,
-	                                      Source src) {
-		return route(GET("/reservations/names"), req -> {
-
-			Flux<String> names = client
-					.get()
-					.uri("http://reservation-service/reservations")
-					.retrieve()
-					.bodyToFlux(Reservation.class)
-					.map(Reservation::getReservationName);
-
-			Publisher<String> fallback = HystrixCommands
-					.from(names)
-					.commandName("reservation-names")
-					.fallback(Flux.just("EEK!"))
-					.eager()
+		@Bean
+		WebClient webClient(LoadBalancerExchangeFilterFunction eff) {
+				ExchangeFilterFunction fallback = (clientRequest, exchangeFunction) -> {
+						try {
+								return eff.filter(clientRequest, exchangeFunction);
+						}
+						catch (Exception e) {
+								return Mono.just(ClientResponse.create(HttpStatus.SERVICE_UNAVAILABLE).build());
+						}
+				};
+				return WebClient.builder()
+					.filter(fallback)
 					.build();
+		}
 
-			return ServerResponse.ok().body(fallback, String.class);
-		})
-				.andRoute(POST("/reservations"), req -> {
-							Flux<Boolean> sendResult = req.bodyToFlux(Reservation.class)
+		@Bean
+		RouterFunction<ServerResponse> routes(WebClient client,
+																																								Source src) {
+				return route(GET("/reservations/names"), req -> {
+
+						Flux<String> names = client
+							.get()
+							.uri("http://reservation-service/reservations")
+							.retrieve()
+							.bodyToFlux(Reservation.class)
+							.map(Reservation::getReservationName);
+
+						Publisher<String> fallback = HystrixCommands
+							.from(names)
+							.commandName("reservation-names")
+							.fallback(Flux.just("EEK!"))
+							.eager()
+							.build();
+
+						return ServerResponse.ok().body(fallback, String.class);
+				})
+					.andRoute(POST("/reservations"), req -> {
+								Flux<Boolean> sendResult = req.bodyToFlux(Reservation.class)
 									.map(Reservation::getReservationName)
 									.map(r -> MessageBuilder.withPayload(r).build())
 									.map(msg -> src.output().send(msg));
-							return ServerResponse.ok().body(sendResult, Boolean.class);
+								return ServerResponse.ok().body(sendResult, Boolean.class);
 						}
-				);
-	}
+					);
+		}
 
-	@Bean
-	SecurityWebFilterChain authorization(ServerHttpSecurity security) {
-		//@formatter:off
+		@Bean
+		SecurityWebFilterChain authorization(ServerHttpSecurity security) {
+				//@formatter:off
 		return
 				security
 				.csrf().disable()
@@ -105,25 +116,25 @@ public class ReservationClientApplication {
 				.and()
 				.build();
 		//@formatter:on
-	}
+		}
 
-	@Bean
-	RouteLocator gateway(RouteLocatorBuilder rlb,
-	                     RedisRateLimiter rl) {
-		return rlb
-				.routes()
-				.route(rs -> rs
+		@Bean
+		RouteLocator gateway(RouteLocatorBuilder rlb,
+																							RedisRateLimiter rl) {
+				return rlb
+					.routes()
+					.route(rs -> rs
 						.path("/proxy")
 						.filters(fs -> fs
-								.requestRateLimiter(c -> c.setRateLimiter(rl))
-								.setPath("/reservations"))
+							.requestRateLimiter(c -> c.setRateLimiter(rl))
+							.setPath("/reservations"))
 						.uri("lb://reservation-service/"))
-				.build();
-	}
+					.build();
+		}
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReservationClientApplication.class, args);
-	}
+		public static void main(String[] args) {
+				SpringApplication.run(ReservationClientApplication.class, args);
+		}
 }
 
 @Data
@@ -131,7 +142,7 @@ public class ReservationClientApplication {
 @AllArgsConstructor
 class Reservation {
 
-	private String id;
+		private String id;
 
-	private String reservationName;
+		private String reservationName;
 }
